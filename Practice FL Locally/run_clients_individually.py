9bb3ea1e5822ba692d1102e2c4fee38bee8ebe69
Mohammed Sunain <mdsunain218@gmail.com>
2026-01-03 15:57:45 +0530

    Practice_federated_learning_fedavg
    Implement federated learning with FedAvg and multi-round training

---
+++ Practice FL Locally/run_clients_individually.py
@@ -0,0 +1,7 @@
+# Optional: Run this to understand how to run clients individually
+
+from client_train import train_client
+
+train_client("federated_data/client1", "client1")
+train_client("federated_data/client2", "client2")
+train_client("federated_data/client3", "client3")
